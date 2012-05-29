212bb5ccf1ed334e4478f5c1a9845e9b84f982fd
Marina Samuel <marina.samuel25@gmail.com>
2012-05-28 20:41:04 -0400

    Final commit used for collecting data.

--- a1.cc
+++ a1.cc
@@ -15,90 +15,87 @@
 using namespace std;
 
 // Constants:
-const long TICKS = 6000000000;
-const double LAMBDA = 100;     // Avg # of packets generated/arrived per second
-const double LENGTH = 1;       // Length of packet in bits
-const double C = 1;            // Transmittion rate of the output link (bits per second)
+const unsigned long TICKS = 5000000000;
+const double LAMBDA = 100;        // Avg # of packets arrived per second
+const double LENGTH = 1000;       // Length of packet in bits
+const double C = 1000000;         // Transmittion rate of the output link (bits per second)
+
+const int SERVICE_TIME = LENGTH * 1000 / C;
 
 // Output variables.
-double numPackets = 0;         // Sum of packets in every tick.
-double totalTime = 0;          // Sum of time taken from arrival to departure for every packet.
-double totalIdleTicks = 0;     // Total num ticks with server doing nothing.
-//const double p;              // Utilization of the queue
+double numPackets = 0;            // Sum of packets in every tick.
+double totalTime = 0;             // Sum of time taken from arrival to departure for every packet.
+double totalIdleTicks = 0;        // Total num ticks with server doing nothing.
+//const double p;                 // Utilization of the queue
 
 // Other.
-int T_ARRIVAL;                 // Time for next packet arrival.
-int T_DEPARTURE;               // Time for next packet departure.
-int T_TRANSMIT;                // Time for next packet transmit.
-int packetNum = 0;             // Packet data.
-queue<int> q;                  // Router queue.
-queue<int> elapsedTime;        // A queue of the arrival times of each packet.
+unsigned long T_ARRIVAL;          // Time for next packet arrival.
+unsigned long T_DEPARTURE;        // Time for next packet departure.
+unsigned long T_TRANSMIT;         // Time for next packet transmit.
+unsigned long packetNum = 0;      // Packet data.
+queue<int> q;                     // Router queue.
+queue<unsigned long> elapsedTime; // A queue of the arrival times of each packet.
 
 double generateMicrosecondX() {
-  return (-1 / LAMBDA) * log(1 - genrand()) * 1000000;
+  // Multiply to convert to microseconds.
+  // Add 1 to ensure generated number is > 0.
+  return (-1.0 / LAMBDA) * log(1.0 - genrand()) * 1000000.0 + 1.0;
 }
 
-void arrival(long t) {
+void arrival(unsigned long t) {
   if (t != T_ARRIVAL) {
     return;
   }
 
-  cout << "New packet will be: " << packetNum << endl;
+  //cout << "New packet will be: " << packetNum << endl;
   q.push(packetNum++);
   elapsedTime.push(t);
   T_ARRIVAL += generateMicrosecondX();
 }
 
-void departure(long t) {
+void departure(unsigned long t) {
   // Server is idle only when the queue is empty.
   if (q.size() == 0) {
     totalIdleTicks++;
   }
 
+  if (t == T_TRANSMIT) {
+    //cout << "Packet " << q.front() << " is departing." << endl;
+    q.pop();
+
+    // Add the time taken from arrival to end of transmission.
+    totalTime += (t-elapsedTime.front());
+    elapsedTime.pop();
+  }
+
   // There are items in the queue but nothing is scheduled to transmit yet.
-  if (q.size() != 0 && t > T_TRANSMIT) {
+  if (q.size() != 0 && t >= T_TRANSMIT) {
     T_DEPARTURE = t;
-    T_TRANSMIT = T_DEPARTURE + LENGTH / C;
-  }
-
-  if (t != T_TRANSMIT) {
-    return;
-  }
-
-  cout << "Packet " << q.front() << " is departing." << endl;
-  q.pop();
-
-  // Add the time taken from arrival to end of transmission.
-  totalTime += (t-elapsedTime.front());
-  elapsedTime.pop();
-
-  if (q.size() != 0) {
-    T_DEPARTURE = t;
-    T_TRANSMIT = T_DEPARTURE + LENGTH / C;
+    T_TRANSMIT = T_DEPARTURE + SERVICE_TIME;
   }
 }
 
-void start_simulation(long ticks) {
-  for (long t = 0; t < ticks; t++) {
+void start_simulation(unsigned long ticks) {
+  for (unsigned long t = 0; t < ticks; ++t) {
     numPackets += q.size();
-    usleep(1);
     arrival(t);
     departure(t);
+    //if (t % 1000000 == 0) cout << "tick: " << t << endl;
   }
 }
 
 void compute_performances() {
-  cout << fixed << setprecision(2);
+  cout << fixed << setprecision(10);
   cout << "E[N]: " << numPackets / TICKS << " (avg. # packets in queue per tick)" << endl;
   cout << "E[T]: " << totalTime / TICKS << " (avg. # ticks from arrival to end of transmission)" << endl;
   cout << "Pidle: " << (totalIdleTicks / TICKS) * 100 << " %"<< endl;
 }
 
 int main() {
-  sgenrand(4357);
+  sgenrand(1359084);
   T_ARRIVAL = generateMicrosecondX();
   T_DEPARTURE = T_ARRIVAL;
-  T_TRANSMIT = T_DEPARTURE + LENGTH / C;
+  T_TRANSMIT = T_DEPARTURE + SERVICE_TIME;
 
   cout << "Arrival Tick: " << T_ARRIVAL << endl;
   cout << "Departure Tick: " << T_DEPARTURE << endl;
